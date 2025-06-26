6d29112f02605a9a1bb9fdc1211493ff1ed2c559
Sharon Drury <98319755+sharondrury@users.noreply.github.com>
2025-06-26 10:27:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <form>
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
